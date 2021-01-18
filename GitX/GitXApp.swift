be4174fbccb1c34ec6879cc0f507f5724dc0e8ce
Tiffany <chingnokyee@gmail.com>
2021-01-18 22:31:05 +0800

    Initial Commit

---
+++ GitX/GitXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXApp.swift
+//  GitX
+//
+//  Created by Tiffany on 18/1/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
